fc60eaf7921b04f82e15fbcea3b331ec175cbdb9
Jonathan Minton <nate.minton@gmail.com>
2014-11-20 15:35:07 +0000

    remove na rows from long data

--- Scripts/gavin_function.R
+++ Scripts/gavin_function.R
@@ -24,6 +24,8 @@ all_pollution_data_long <- melt(
     
     ) 
 
+na_values <- is.na(all_pollution_data_long$value)
+all_pollution_data_long <- all_pollution_data_long[!na_values,]
 
 str(datazones_centroids)
 
@@ -44,78 +46,78 @@ gavin_linkage_fn <- function(
     #@ 2: yearly pollution data; the names is PD.data, otherwise we need change the names within the function
     #@ 3: make sure there is no missing data for each pollutant in the Pollutant grid point data
     #  Finally, we can directly merge the calculated pollution data to the Scottish Data Zone
+    
     num_pollutant_obs <- nrow(pollution_data)
     num_dzs <- nrow(dz_centroids) # the number of data zones in Scotland
     out <- rep(NA, length=num_dzs)
-         # Temporally keep the range parameters through the simulations
-        range_temp <- rep(0,range_param)
-        for (i in 1:range_param) {
-            # draw samples
-            temp_pollution_data <- pollution_data[
-                sample(
-                    1:nrow(pollution_data),
-                    min(num_samples, num_pollutant_obs),
-                    rep=FALSE
-                ),
-                ]
-            
-            # Note the distance measured using KM
-            # keep the THIRD and FOURTH columns are coordinates of the yearly pollution data
-            dd <- variog(
-                coords=temp_pollution_data[,c("x","y")]/1000,
-                data=temp_pollution_data[,"value"], 
-                messages=FALSE
-                )
-            
-            dd_fit <- variofit(
-                dd,
-                cov.model="gaussian",
-                messages=FALSE
-                )
-            
-            range_temp[i] <- dd_fit$cov.pars[2]
-            if (i %% 10 ==0) print(i)
-        }
-        range <- as.numeric(mean(range_temp))
-        # assume a 0.7 correlation threshold; can change but need to aware that smaller threshold implies longer computing time
-        threshold_dist <- range*sqrt(-log(0.7))
+    # Temporally keep the range parameters through the simulations
+    range_temp <- rep(0,range_param)
+    for (i in 1:range_param) {
+        # draw samples
+        temp_pollution_data <- pollution_data[
+            sample(
+                1:nrow(pollution_data),
+                min(num_samples, num_pollutant_obs),
+                rep=FALSE
+            ),
+            ]
         
+        # Note the distance measured using KM
+        # keep the THIRD and FOURTH columns are coordinates of the yearly pollution data
+        dd <- variog(
+            coords=temp_pollution_data[,c("x","y")]/1000,
+            data=temp_pollution_data[,"value"], 
+            messages=FALSE
+        )
         
-        ######## Step 3
+        dd_fit <- variofit(
+            dd,
+            cov.model="gaussian",
+            messages=FALSE
+        )
         
-        # The indices of srounding pollution data points
-        #ind <- list()
-        # weights through a Gaussian kernel as we did in Step 2
-        #Gweight <- list()
-        coord_temp <- as.matrix(pollution_data[,c("x", "y")]/1000)
+        range_temp[i] <- dd_fit$cov.pars[2]
+        if (i %% 10 ==0) print(i)
+    }
+    range <- as.numeric(mean(range_temp))
+    # assume a 0.7 correlation threshold; can change but need to aware that smaller threshold implies longer computing time
+    threshold_dist <- range*sqrt(-log(0.7))
+    
+    
+    ######## Step 3
+    
+    # The indices of srounding pollution data points
+    #ind <- list()
+    # weights through a Gaussian kernel as we did in Step 2
+    #Gweight <- list()
+    coord_temp <- as.matrix(pollution_data[,c("x", "y")]/1000)
+    
+    # the range parameter obtained in Step 2. We also assume it to be the
+    # the threshold of distance beyond which there is no spatial correlations
+    # although it is not entirely true as the spatial correlation is about exp{-1} = 0.37
+    
+    for (i in 1:num_dzs) {
+        # First calculate the distances between each dz and all pollution data
+        dist_temp <- spDistsN1(
+            pts=coords.temp,
+            pt=DZ_centroid@coords[i,]/1000
+        )
+        # Change distance to geographical weights using parameters calibrated in Step 2
+        ind_temp <- which(
+            dist_temp <= threshold_dist,
+            arr.ind=TRUE
+        )
+        # Gaussian kernel
+        w_temp <- exp(-(dist_temp[ind_temp]/range)^2)
         
-        # the range parameter obtained in Step 2. We also assume it to be the
-        # the threshold of distance beyond which there is no spatial correlations
-        # although it is not entirely true as the spatial correlation is about exp{-1} = 0.37
-        
-        for (i in 1:num_dzs) {
-            # First calculate the distances between each dz and all pollution data
-            dist_temp <- spDistsN1(
-                pts=coords.temp,
-                pt=DZ_centroid@coords[i,]/1000
-                )
-            # Change distance to geographical weights using parameters calibrated in Step 2
-            ind_temp <- which(
-                dist_temp <= threshold_dist,
-                arr.ind=TRUE
-                )
-            # Gaussian kernel
-            w_temp <- exp(-(dist_temp[ind_temp]/range)^2)
-            
-            out[i] <- sum(as.numeric(pollution_data[ind_temp]*w_temp))/sum(w_temp)
-            if(i %% 1000 == 0) print(i)
-        }
+        out[i] <- sum(as.numeric(pollution_data[ind_temp]*w_temp))/sum(w_temp)
+        if(i %% 1000 == 0) print(i)
+    }
         
         # This takes about 1.7 minutes
         # finish the calculation of the k-th pollutant
-
+        
     return(out)
-    
 }
 
 debug(gavin_linkage_fn)
